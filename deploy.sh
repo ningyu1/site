e2a6b9de2aa7c53e39f43086dcd9cfdce39ae8da
ningyu1 <ningbe111@163.com>
2020-12-31 10:31:58 +0800

    add travis-ci

---
+++ deploy.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env sh
+
+# 确保脚本抛出遇到的错误
+set -e
+
+# 生成静态文件
+hugo --buildDrafts
+
+# 进入生成的文件夹
+cd ./public
+
+#创建.nojekyll 防止Github Pages build错误
+touch .nojekyll
+
+git init
+git config user.name "ningyu1"
+git config user.email "ningbe111@163.com"
+git add -A
+git commit -m "Update Blog By TravisCI With Build $TRAVIS_BUILD_NUMBER"
+git push -f "https://${access_token}@github.com/ningyu1/site.git" gh-pages:gh-pages
+
+cd -
